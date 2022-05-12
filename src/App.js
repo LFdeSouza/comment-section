1c3235e9e29a4b666b5a5d96830f80cc35eb0d94
LFS1245 <luiz.f.souza@live.com>
2022-05-12 11:50:32 -0300

    comments responsiveness and replies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Comment from "./components/Comment";
+import jsonData from "./data.json";
 
 function App() {
+  if (!localStorage.getItem("commentSection")) {
+    localStorage.setItem("commentSection", JSON.stringify(jsonData));
+  }
+  const data = JSON.parse(localStorage.getItem("commentSection"));
+  const comments = data.comments;
+
   return (
-    <section className="h-screen w-screen bg-lightGray pt-8">
-      <Comment />
+    <section className="min-w-screen min-h-screen bg-lightGray pt-8">
+      {comments &&
+        comments.map((comment) => (
+          <Comment
+            content={comment.content}
+            createdAt={comment.createdAt}
+            user={comment.user}
+            score={comment.score}
+            replies={comment.replies}
+          />
+        ))}
     </section>
   );
 }
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import picture from "../assets/images/avatars/image-amyrobson.png";
 
-const Comment = ({ user, content, date, score }) => {
+const Comment = ({ user, content, createdAt, score, replies }) => {
+  //import image;
+
   const iconPlus = (
     <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
       <path
@@ -28,29 +29,54 @@ const Comment = ({ user, content, date, score }) => {
       />
     </svg>
   );
-  return (
-    <div className="mx-auto w-11/12 rounded-md bg-white p-4">
-      <div className="flex items-center gap-4">
-        <img src={picture} alt="avatar" className="h-8 w-8" />
-        <h1 className="font-bold">amyrobson</h1>
-        <p className="text-gray-500">1 month ago</p>
-      </div>
-      <p className="mt-4 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-        adipisci? Maiores unde omnis enim nesciunt harum, molestiae, ipsam
-        quisquam praesentium voluptatibus eveniet laudantium placeat!
-        Necessitatibus ullam voluptatem possimus velit sequi.
-      </p>
 
-      <div className="mt-2 flex  items-center justify-between">
-        <div className="flex w-fit items-center justify-center gap-4 rounded-lg bg-lightGray py-2 px-4">
-          <i className="cursor-pointer">{iconPlus}</i>
-          <p className=" font-bold text-moderateBlue">12</p>
-          <i className="cursor-pointer">{iconMinus}</i>
+  return (
+    <div className="mx-auto max-w-2xl">
+      <div className="mx-auto flex w-11/12 flex-col rounded-md bg-white p-4 sm:flex-row sm:items-start sm:gap-4 sm:p-6 md:w-full">
+        <div className="sm:order-2">
+          <div className="flex items-center gap-4">
+            <img
+              src={require(`../assets/images/avatars/image-${user.username}.png`)}
+              alt="avatar"
+              className="h-10 w-10"
+            />
+            <h1 className="font-bold">{user.username}</h1>
+            <p className="text-gray-500">{createdAt}</p>
+            <p className="ml-auto hidden cursor-pointer items-center gap-2 font-bold text-moderateBlue sm:flex">
+              {iconReply}Reply
+            </p>
+          </div>
+          <p className="mt-4 text-gray-500">{content}</p>
         </div>
-        <p className="flex cursor-pointer items-center gap-2 font-bold text-moderateBlue">
-          {iconReply}Reply
-        </p>
+
+        <div className="mt-2 flex items-center justify-between">
+          <div className="flex w-fit items-center justify-center gap-4 rounded-lg bg-lightGray py-2 px-4 sm:min-h-[7.5rem] sm:flex-col sm:p-2">
+            <i className=" flex h-4 w-4 cursor-pointer items-center justify-center">
+              {iconPlus}
+            </i>
+            <p className=" font-bold text-moderateBlue">{score}</p>
+            <i className="flex h-4 w-4 cursor-pointer items-center justify-center">
+              {iconMinus}
+            </i>
+          </div>
+          <p className="flex cursor-pointer items-center gap-2 font-bold text-moderateBlue sm:hidden">
+            {iconReply}Reply
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-5 ml-6 border-l-2 border-lightGrayishBlue sm:ml-10 sm:pl-10">
+        {replies &&
+          replies.map((reply, index) => (
+            <Comment
+              user={reply.user}
+              content={reply.content}
+              createdAt={reply.createdAt}
+              score={reply.score}
+              replies={[]}
+              key={index}
+            />
+          ))}
       </div>
     </div>
   );
